4a2e30860b7ef58ee4bb1fab1226e5041223afb7
LENOVO <LENOVO@LAPTOP-803E4M6S>
2021-01-30 16:03:36 +0530

    ticket no 98789

---
+++ src/main/java/com/app/hotel/mohel/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.app.hotel.mohel;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Welcome to Spring boot Application");
+		
+	}
+
+}
